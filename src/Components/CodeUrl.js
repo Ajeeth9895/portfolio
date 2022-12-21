e987f4f58fda0f30f66fc392238495eedbf848fa
Ajee9895 <ajeethkumar.gopal@gmail.com>
2022-12-21 17:52:07 +0530

    second commit

---
+++ src/Components/CodeUrl.js
@@ -0,0 +1,14 @@
+
+
+export const CodeUrl = {
+    pokemonGit:"https://github.com/Ajeeth9895/pokemon-pagination",
+    pokemonDeploy:"https://delightful-entremet-a1dc46.netlify.app",
+    weatherGit:"https://github.com/Ajeeth9895/Weather.git",
+    weatherDeploy:"https://fabulous-treacle-da9855.netlify.app",
+    pincodeGit:"https://github.com/Ajeeth9895/Pincode.git",
+    pincodeDeploy:"https://eloquent-liger-6f5816.netlify.app",
+    blank:"_blank"
+     
+}
+
+
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,32 +1,33 @@
 import React from "react";
-import Pokemon from "../Asset/img/pokemon.jpg";
+import Pokemon from "../Asset/img/pokemon.png";
+import weather from "../Asset/img/weather.png";
+import Postalcode from "../Asset/img/Postalcode.png";
 import { Button } from "react-bootstrap";
+import { CodeUrl } from "./CodeUrl";
 
 function Projects() {
   return (
     <div class="projects-d" id="Projects">
-      <div class="container">
+      <div class="container-fluid">
         <div class="text-center">
           <div class="project-details">Projects</div>
         </div>
-        <div class="row post-grid">
-          <div class="col-md-6 col-lg-4 card-box my-5 ">
-            <div class="proj-hd">Pokemon Api</div>
+        <div class="row post-grid d-flex justify-content-center ">
+          <div class="col-md-6 col-lg-3 card-box my-5 mx-4">
+            <div class="proj-hd">Pokemon</div>
             <div class="d-flex justify-content-center my-5">
               <img src={Pokemon} className="proj-img" alt="" />
             </div>
             <div class="proj-button">
-              <a
-                href="https://github.com/Ajeeth9895/pokemon-pagination"
-                target={"_blank"}
-              >
+              <a href={CodeUrl.pokemonGit} target={CodeUrl.blank}>
                 <Button type="button" variant="outline-success">
                   Source
                 </Button>
               </a>
               <a
-                href="https://github.com/Ajeeth9895/pokemon-pagination"
-                target={"_blank"}
+                href={CodeUrl.pokemonDeploy}
+                target={CodeUrl.blank}
+                class="m-2"
               >
                 <Button type="button" variant="outline-success">
                   Deploy
@@ -34,26 +35,47 @@ function Projects() {
               </a>
             </div>
           </div>
-          <div class="col-md-6 col-lg-4 ">
-            {/* <div class="card">
-            <div class="img-place">
-              <img src="../assets/img/work/Weather.jpg" style={{height:"16em"}} alt=""/>
+
+          <div class="col-md-6 col-lg-3 card-box my-5 mx-4">
+            <div class="proj-hd">Weather</div>
+            <div class="d-flex justify-content-center my-5">
+              <img src={weather} className="proj-img" alt="" />
             </div>
-            <div class="caption">
-              <div  class="post-category">Weather Api</div>
-            
+            <div class="proj-button">
+              <a href={CodeUrl.weatherGit} target={CodeUrl.blank}>
+                <Button type="button" variant="outline-success">
+                  Source
+                </Button>
+              </a>
+              <a
+                href={CodeUrl.weatherDeploy}
+                target={CodeUrl.blank}
+                class="m-2"
+              >
+                <Button type="button" variant="outline-success">
+                  Deploy
+                </Button>
+              </a>
             </div>
-          </div> */}
           </div>
-          <div class="col-md-6 col-lg-4 ">
-            {/* <div class="card">
-            <div class="img-place">
-              <img src="../assets/img/work/pincode.jpg"style={{height:"16em"}} alt=""/>
+
+          <div class="col-md-6 col-lg-3 card-box my-5 mx-4">
+            <div class="proj-hd">Postal code</div>
+            <div class="d-flex justify-content-center my-5">
+              <img src={Postalcode} className="proj-img" alt="" />
             </div>
-            <div class="caption">
-              <div  class="post-category">Pincode Api</div>
+            <div class="proj-button">
+              <a href={CodeUrl.pincodeGit} target={CodeUrl.blank}>
+                <Button type="button" variant="outline-success">
+                  Source
+                </Button>
+              </a>
+              <a href={CodeUrl.pincodeDeploy} target={CodeUrl.blank} class="m-2">
+                <Button type="button" variant="outline-success">
+                  Deploy
+                </Button>
+              </a>
             </div>
-          </div> */}
           </div>
         </div>
       </div>
